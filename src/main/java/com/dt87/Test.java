26334d7f93c515d88094db351486b834fa0e134b
huHang-huHang <13297932775@163.com>
2020-05-27 09:57:23 +0800

    test

---
+++ src/main/java/com/dt87/Test.java
@@ -0,0 +1,13 @@
+package com.dt87;
+
+/**
+ * @description:
+ * @author: Mr.HuHang
+ * @create: 2020-05-27 09:44
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git-test");
+    }
+
+}
